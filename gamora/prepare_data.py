c6600349e6b6bad5969df07b89a3945161a2f124
dima sivov <dsivov@gmail.com>
2022-12-15 13:57:22 +0200

    add preprocess to sqlite

---
+++ gamora/prepare_data.py
@@ -0,0 +1,15 @@
+import sqlite3
+import os
+
+def create_sqlite(data_folder, schema_name):
+    con = sqlite3.connect(os.path.join(data_folder, "asana_small.sqlite"))
+    cur = con.cursor()
+    with open(os.path.join(data_folder, schema_name), 'r') as sql_file:
+        result_iterator = cur.executescript(sql_file.read())
+    con.commit()
+
+if __name__ == "__main__":
+    print('Prepare tables data required for running RASAT')
+    data_folder = '/app/dataset_files/ori_dataset/asana/database/asana/'
+    asana_small_scheme = 'Dump20221214.sql'
+    create_sqlite(data_folder=data_folder, schema_name=asana_small_scheme)
