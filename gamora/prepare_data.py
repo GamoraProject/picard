2606dbe2ab521b33fab85b161b810a45e5d06a0c
dima sivov <dsivov@gmail.com>
2022-12-19 15:52:05 +0200

    prepare_sql

--- gamora/prepare_data.py
+++ gamora/prepare_data.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 
 def create_sqlite(data_folder, schema_name):
-    con = sqlite3.connect(os.path.join(data_folder, "asana_small.sqlite"))
+    con = sqlite3.connect(os.path.join(data_folder, "asana1.sqlite"))
     cur = con.cursor()
     with open(os.path.join(data_folder, schema_name), 'r') as sql_file:
         result_iterator = cur.executescript(sql_file.read())
@@ -10,6 +10,6 @@ def create_sqlite(data_folder, schema_name):
 
 if __name__ == "__main__":
     print('Prepare tables data required for running RASAT')
-    data_folder = '/app/dataset_files/ori_dataset/asana/database/asana/'
+    data_folder = '.'
     asana_small_scheme = 'Dump20221214.sql'
     create_sqlite(data_folder=data_folder, schema_name=asana_small_scheme)
